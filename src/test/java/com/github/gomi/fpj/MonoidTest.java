10a9c732c6f69d87d3fcb593e786f502793b65f1
53nigen <korun24276@gmail.com>
2015-06-07 15:58:12 +0900

    refinement

--- src/test/java/com/github/gomi/fpj/MonoidTest.java
+++ src/test/java/com/github/gomi/fpj/MonoidTest.java
@@ -11,28 +11,36 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Theories.class)
 public class MonoidTest {
 
+    <A> void associativity(final Monoid<A> m, final A a1, final A a2, final A a3) {
+        assertEquals(m.op(a1, m.op(a2, a3)), m.op(m.op(a1, a2), a3));
+    }
+
+    <A> void identity(final Monoid<A> m, final A a) {
+        assertEquals(m.op(a, m.zero()), m.op(m.zero(), a));
+    }
+
     @Theory
     public void intAssociativity(@ForAll Integer i1, @ForAll Integer i2, @ForAll Integer i3) {
-        assertEquals(intAddition.op(i1, intAddition.op(i2, i3)), intAddition.op(intAddition.op(i1, i2), i3));
-        assertEquals(intMultiplication.op(i1, intMultiplication.op(i2, i3)), intMultiplication.op(intMultiplication.op(i1, i2), i3));
+        associativity(intAddition, i1, i2, i3);
+        associativity(intMultiplication, i1, i2, i3);
     }
 
     @Theory
     public void intIdentity(@ForAll Integer i1) {
-        assertEquals(intAddition.op(i1, intAddition.zero()), intAddition.op(intAddition.zero(), i1));
-        assertEquals(intMultiplication.op(i1, intMultiplication.zero()), intMultiplication.op(intMultiplication.zero(), i1));
+        identity(intAddition, i1);
+        identity(intMultiplication, i1);
     }
 
     @Theory
     public void booleanAssociativity(@ForAll Boolean b1, @ForAll Boolean b2, @ForAll Boolean b3) {
-        assertEquals(booleanOr.op(b1, booleanOr.op(b2, b3)), booleanOr.op(booleanOr.op(b1, b2), b3));
-        assertEquals(booleanAnd.op(b1, booleanAnd.op(b2, b3)), booleanAnd.op(booleanAnd.op(b1, b2), b3));
+        associativity(booleanOr, b1, b2, b3);
+        associativity(booleanAnd, b1, b2, b3);
     }
 
     @Theory
     public void booleanIdentity(@ForAll Boolean b1) {
-        assertEquals(booleanOr.op(b1, booleanOr.zero()), booleanOr.op(booleanOr.zero(), b1));
-        assertEquals(booleanAnd.op(b1, booleanAnd.zero()), booleanAnd.op(booleanAnd.zero(), b1));
+        identity(booleanOr, b1);
+        identity(booleanAnd, b1);
     }
 
 }
